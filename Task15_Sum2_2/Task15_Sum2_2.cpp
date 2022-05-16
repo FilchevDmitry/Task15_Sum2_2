9ff1622362b8848b1480439cabb38f023c1bd8a8
DmityFilchev <filchev_dmitry@mail.ru>
2022-05-16 16:20:43 +0300

    Добавьте файлы проекта.

---
+++ Task15_Sum2_2/Task15_Sum2_2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+	vector<int> a = { 2,7,11,15 };
+	int n = 9;
+	int one = 0, two = 0;
+	for (int i = 0; i < a.size(); i++ )
+	{
+		for (int j = 1; j < a.size(); j++)
+		{
+			if (a[i] + a[j] == n)
+			{
+				one = a[i];
+				two =a[j];
+			}
+		}
+	}
+	cout << one << " " << two << endl;
+}
